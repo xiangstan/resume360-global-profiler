b08a9b3d38b0960b18cda5d16a7aa8190527cf9f
Xiang S Tan <xtan@uccs.edu>
2024-05-15 19:50:51 -0600

    Updated the footer template.

--- src/components/static/FooterView.vue
+++ src/components/static/FooterView.vue
@@ -1,7 +1,31 @@
 <template>
-  <footer class="bg-gray-700 text-gray-100">
-    <div class="max-w-6xl mx-auto px-4 py-6">
-      This is the Footer.
+  <footer class="bg-gray-700 text-gray-100 py-8">
+    <div class="max-w-6xl mx-auto px-4">
+      <div class="flex flex-col md:flex-row justify-between items-center md:items-start">
+        <!-- Left Column -->
+        <div class="text-center md:text-left mb-4 md:mb-0">
+            <h2 class="text-xl font-bold">Resume360 Global Profiler</h2>
+            <p class="mt-2 max-w-xs">Resume360 Global Profiler helps you create professional resumes and profiles that stand out in the global market.</p>
+        </div>
+        <!-- Right Column -->
+        <div class="flex justify-center md:justify-end space-x-4">
+            <a href="https://www.facebook.com" target="_blank" class="social-link">
+                <img src="@/assets/img/facebook.svg" alt="Facebook" class="h-8 w-8">
+            </a>
+            <a href="https://www.twitter.com" target="_blank" class="social-link">
+                <img src="@/assets/img/twitter.svg" alt="Twitter" class="h-8 w-8">
+            </a>
+            <a href="https://www.discord.com" target="_blank" class="social-link">
+                <img src="@/assets/img/discord.svg" alt="Discord" class="h-8 w-8">
+            </a>
+            <a href="https://www.linkedin.com" target="_blank" class="social-link">
+                <img src="@/assets/img/linkedin.svg" alt="LinkedIn" class="h-8 w-8">
+            </a>
+        </div>
+      </div>
+      <div class="text-center mt-4">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+      </div>
     </div>
   </footer>
 </template>
