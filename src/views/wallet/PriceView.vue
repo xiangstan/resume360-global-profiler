1616677c12b3e7c37ec591ab0b11439a19c0418b
Xiang S Tan <xtan@uccs.edu>
2024-05-26 11:39:25 -0600

    Updated the wallet view template. Added price display of ETH and Link.

---
+++ src/views/wallet/PriceView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="border-2 bg-slate-200 dark:bg-slate-500 px-5 py-3 mb-3 rounded-md">
+    <strong class="uppercase">{{ token }}</strong>: ${{ price }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  token: {
+    type: String,
+    default: ''
+  },
+  price: {
+    type: Number,
+    default: 0
+  }
+})
+</script>
